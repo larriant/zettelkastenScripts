3cafd759493629a10640376af8a924020fc30dbf
Julyan Davey <julyan@near.st>
2018-06-16 20:21:40 +0200

    Extracting Title and Answer from Cards

---
+++ textToCards.py
@@ -0,0 +1,33 @@
+import os
+
+for filename in os.listdir('../'):
+    if filename.endswith(".md") and filename == '201805161546 Entropic Brain.md':
+        timestamp = filename[0:12]
+        if timestamp.isdigit():
+            title = filename[12:-3]
+            print('Timestamp', timestamp)
+            print('Title:', title)
+            
+            file_object = open('../' + filename, 'r')
+            lines = file_object.readlines()
+            numLines = len(lines)
+            # print(numLines)
+            more = False
+            checkLine = 6
+            moreLine = 0
+            # print(lines[checkLine])
+
+            while more is False and checkLine < numLines:
+                line = lines[checkLine].strip()
+                # print(checkLine)
+                # print('1',line,'2')
+                # print(len(line))
+                if line == '# More':
+                    # print('FOUND AN ALSO LINE')
+                    more = True
+                    moreLine = checkLine
+                else:
+                    checkLine += 1
+            print('Answer:', lines[6:moreLine])
+            print(' ')
+            print(' ')
