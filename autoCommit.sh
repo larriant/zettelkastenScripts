c213b68cc6c0c843394c4039de42ed0ea310f8f2
Julyan Davey <julyan@near.st>
2018-06-17 16:00:40 +0200

    Auto Commit now working

--- autoCommit.sh
+++ autoCommit.sh
@@ -1,10 +1,8 @@
 cd ~/Dropbox/Zettelkasten
 if [[ `git status --porcelain` ]]; then
   # Changes
-  echo 'Changes'
   git add -A
   git commit -m "Update"
 else
   # No changes
-  echo 'No Changes'
 fi
